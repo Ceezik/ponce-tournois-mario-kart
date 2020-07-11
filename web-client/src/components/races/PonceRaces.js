956da5064ca92bc042a35987cef998f111fc3360
Ceezik <ceezik.pro@gmail.com>
2020-07-11 10:37:34 +0200

    WEB CLIENT : show Ponce's races

---
+++ web-client/src/components/races/PonceRaces.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+import _ from 'lodash';
+import { useSocket } from '../../utils/useSocket';
+import { useTracks } from '../../utils/useTracks';
+import { getAll } from '../../services/cups';
+import RacesListItem from './RacesListItem';
+import RacesSkeleton from './RacesSkeleton';
+
+function PonceRaces() {
+    const { socket } = useSocket();
+    const { tracks } = useTracks();
+    const [races, setRaces] = useState([]);
+    const [cups, setCups] = useState([]);
+    const [loadingRaces, setLoadingRaces] = useState(true);
+    const [loadingCups, setLoadingCups] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        socket.on('getPonceRaces', (races) => {
+            setRaces(races);
+            setLoadingRaces(false);
+        });
+
+        socket.emit('getPonceRaces', (err) => {
+            setError(err);
+            setLoadingRaces(false);
+        });
+
+        getAll()
+            .then((res) => setCups(res.data))
+            .catch(() => setError('Impossible de récupérer les coupes'))
+            .finally(() => setLoadingCups(false));
+
+        return () => socket.off('getPonceRaces');
+    }, []);
+
+    useEffect(() => {
+        if (!loadingRaces && !loadingCups) {
+            const newTracks = _.cloneDeep(tracks);
+            const newCups = _.cloneDeep(cups);
+
+            newCups.forEach((cup) => (cup.Tracks = []));
+
+            newTracks.forEach((track) => {
+                const cup = _.find(newCups, { id: track.CupId });
+                track.statistics = {
+                    nbPlayed: 0,
+                    nbPoints: 0,
+                    position: 0,
+                };
+                cup.Tracks.push(track);
+            });
+
+            races.forEach((race) => {
+                const cup = _.find(newCups, { id: race.Track.CupId });
+                const track = _.find(cup.Tracks, { id: race.TrackId });
+                track.statistics.nbPlayed++;
+                track.statistics.nbPoints += race.nbPoints;
+                track.statistics.position += race.position;
+            });
+            setCups(newCups);
+            setLoading(false);
+        }
+    }, [loadingCups, loadingRaces]);
+
+    return (
+        <Container className="app__container">
+            {loading ? (
+                <RacesSkeleton />
+            ) : error ? (
+                <Row justify="center">
+                    <Col xs="content">
+                        <div className="formMessage formMessage__error">
+                            {error}
+                        </div>
+                    </Col>
+                </Row>
+            ) : (
+                <Row justify="center">
+                    <Col xs={12} lg={8} className="races__wrapper">
+                        {cups.map((cup) => (
+                            <RacesListItem key={cup.id} cup={cup} />
+                        ))}
+                    </Col>
+                </Row>
+            )}
+        </Container>
+    );
+}
+
+export default PonceRaces;
