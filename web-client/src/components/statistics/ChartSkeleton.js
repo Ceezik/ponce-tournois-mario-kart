7eed74314ff06b731a9f661ee5772641c3fb7fda
Clément KOENIG <clement.koenig@unistra.fr>
2020-07-11 18:06:44 +0200

    WEB CLIENT : show Ponce's statistics

---
+++ web-client/src/components/statistics/ChartSkeleton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+function ChartSkeleton() {
+    return (
+        <>
+            <h1 className="statistics__title">
+                <Skeleton width="50%" />
+            </h1>
+
+            <Skeleton height="20rem" />
+        </>
+    );
+}
+
+export default ChartSkeleton;
