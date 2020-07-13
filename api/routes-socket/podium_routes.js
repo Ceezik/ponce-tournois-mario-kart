d2126a91648547010fa751761c06e2e408cc38ad
Ceezik <ceezik.pro@gmail.com>
2020-07-13 09:02:37 +0200

    API : get and create podium

---
+++ api/routes-socket/podium_routes.js
@@ -0,0 +1,15 @@
+const podium_ctrl = require('../controllers/podium_ctrl');
+
+module.exports = (io, socket, userId, isAdmin) => {
+    socket.on('getPodium', (tournamentId, onError) => {
+        podium_ctrl.getPodium(socket, onError, tournamentId);
+    });
+
+    socket.on('addPodium', (podium, onError) => {
+        if (isAdmin) {
+            podium_ctrl.create(io, socket, onError, podium);
+        } else {
+            onError("Vous n'êtes pas autorisé à effectuer cette action");
+        }
+    });
+};
