cd2abf56e63b4981b801ca7e1235e94968703baf
Ceezik <ceezik.pro@gmail.com>
2020-07-13 11:51:01 +0200

    WEB CLIENT : footer

---
+++ web-client/src/components/utils/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            Fait avec le 💜 par{' '}
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/Ceezik"
+            >
+                Ceezik
+            </a>
+        </footer>
+    );
+}
+
+export default Footer;
