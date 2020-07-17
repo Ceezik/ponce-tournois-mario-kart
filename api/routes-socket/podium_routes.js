f06243a20a34a51fcc81870048bedcbafb27b08e
Clément KOENIG <clement.koenig@unistra.fr>
2020-07-17 17:32:48 +0200

    API : edit a podium

--- api/routes-socket/podium_routes.js
+++ api/routes-socket/podium_routes.js
@@ -12,4 +12,12 @@ module.exports = (io, socket, userId, isAdmin) => {
             onError("Vous n'êtes pas autorisé à effectuer cette action");
         }
     });
+
+    socket.on('editPodium', (podium, onError) => {
+        if (isAdmin) {
+            podium_ctrl.update(io, socket, onError, podium);
+        } else {
+            onError("Vous n'êtes pas autorisé à effectuer cette action");
+        }
+    });
 };
