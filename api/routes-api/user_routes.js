a93b7c29ade56edbae68522ca24d6b3441f09f3e
Clément KOENIG <clement.koenig@unistra.fr>
2020-07-17 16:06:18 +0200

    API : get all users

--- api/routes-api/user_routes.js
+++ api/routes-api/user_routes.js
@@ -3,6 +3,12 @@ const auth_ctrl = require('../controllers/auth_ctrl');
 
 module.exports = [
     {
+        url: '/users',
+        method: 'get',
+        func: [auth_ctrl.isAuthenticated, auth_ctrl.isAdmin, user_ctrl.getAll],
+    },
+
+    {
         url: '/user',
         method: 'get',
         func: [auth_ctrl.isAuthenticated, user_ctrl.getCurrent],
