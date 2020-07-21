1aebdf8c993cda4048ee002c24eb73d6766ebfa4
Ceezik <ceezik.pro@gmail.com>
2020-07-21 17:17:22 +0200

    WEB CLIENT : fix header on xxl screens

--- web-client/src/components/utils/Header.js
+++ web-client/src/components/utils/Header.js
@@ -16,7 +16,7 @@ function Header() {
             <Hidden xs sm>
                 <DesktopHeader />
             </Hidden>
-            <Hidden md lg xl>
+            <Hidden md lg xl xxl>
                 <MobileHeader />
             </Hidden>
         </header>
@@ -279,7 +279,7 @@ function TwitchButton() {
                 x="0px"
                 y="0px"
             >
-                <g fill-rule="evenodd">
+                <g fillRule="evenodd">
                     <path d="M19 6v6h-2V6h2zm-7 0h2v6h-2V6zM5 0L0 5v18h6v5l5-5h4l9-9V0H5zm17 13l-4 4h-4l-4 4v-4H6V2h16v11z"></path>
                     <path
                         d="M18 17l4-4V2H6v15h4v4l4-4h4zM12 6h2v6h-2V6zm7 0h-2v6h2V6z"
