de035f55edf7769b003ac16d0f4bb2f07e5f8435
Clément KOENIG <clement.koenig@unistra.fr>
2021-01-30 22:38:01 +0100

    API : patch note model

---
+++ api/models/patch_note.js
@@ -0,0 +1,27 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class PatchNote extends Model {}
+
+    PatchNote.init(
+        {
+            version: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: { len: [3, 100] },
+            },
+            content: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'PatchNote',
+            updatedAt: false,
+        }
+    );
+
+    return PatchNote;
+};
