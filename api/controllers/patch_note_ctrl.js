8e6a4a3a14c9213ad706cf158cd6df75b463a9a5
Clément KOENIG <clement.koenig@unistra.fr>
2021-01-31 12:10:38 +0100

    API : create a patch note

--- api/controllers/patch_note_ctrl.js
+++ api/controllers/patch_note_ctrl.js
@@ -8,4 +8,25 @@ module.exports = {
             .then((patchNotes) => res.json(patchNotes))
             .catch((err) => next(err));
     },
+
+    create: (req, res, next) => {
+        const { version, content } = req.body;
+
+        if (version && content) {
+            return db.PatchNote.versionIsUnique(version)
+                .then((isUnique) => {
+                    if (isUnique) {
+                        return db.PatchNote.create({ version, content })
+                            .then((patchNote) => res.json(patchNote))
+                            .catch((err) => next(err));
+                    }
+                    throw {
+                        status: 409,
+                        message: 'Un patch note avec cette version existe déjà',
+                    };
+                })
+                .catch((err) => next(err));
+        }
+        throw { status: 406, message: 'Paramètres invalides' };
+    },
 };
--- api/routes-api/patch_note_routes.js
+++ api/routes-api/patch_note_routes.js
@@ -1,9 +1,20 @@
 const patch_note_ctrl = require('../controllers/patch_note_ctrl');
+const auth_ctrl = require('../controllers/auth_ctrl');
 
 module.exports = [
     {
         url: '/patch-notes',
         method: 'get',
         func: patch_note_ctrl.getAll,
+    },
+
+    {
+        url: '/patch-notes',
+        method: 'post',
+        func: [
+            auth_ctrl.isAuthenticated,
+            auth_ctrl.isAdmin,
+            patch_note_ctrl.create,
+        ],
     },
 ];
