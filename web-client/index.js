f98ab4cfba09351ad059022c84786cb5ea9a784a
Clément KOENIG <clement.koenig@unistra.fr>
2021-02-01 22:07:58 +0100

    WEB CLIENT : serve app with express

---
+++ web-client/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const appPath = path.join(__dirname, "client", "build");
+const port = process.env.PORT || 80;
+
+app.use(express.static(appPath));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(appPath, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server is up on port : ", port);
+});
