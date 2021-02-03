88f83da8c7b419a949d70a80259748b15de450b4
Ceezik <ceezik.pro@gmail.com>
2021-02-03 21:24:51 +0100

    API : run app in clusters

--- api/index.js
+++ api/index.js
@@ -1,46 +1,70 @@
 require('dotenv').config();
-
 const express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io')(server),
+    io = require('socket.io')(server, {
+        transports: ['websocket'],
+    }),
+    redisAdapter = require('socket.io-redis'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     helmet = require('helmet'),
-    PORT = process.env.PORT;
+    cluster = require('cluster'),
+    numCPUs = require('os').cpus().length;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-    cors({
-        origin: [process.env.WEB_CLIENT_URL],
-    })
-);
-app.use(helmet());
+const { NODE_ENV, PORT, REDIS_URL } = process.env;
 
-const errorHandler = (err, req, res, next) => {
-    if (err.status === null || err.status === undefined) {
-        res.status(500).send(err.message);
-    } else {
-        res.status(err.status).send(err.message);
+if (cluster.isMaster && NODE_ENV !== 'test') {
+    console.log(`Master cluster setting up ${numCPUs} workers....`);
+
+    for (let i = 0; i < numCPUs; i += 1) {
+        cluster.fork();
     }
-};
 
-require('./passport');
-require('./routes-api')(app);
+    cluster.on(`online`, (worker) => {
+        console.log(`Worker ${worker.process.pid} is online`);
+    });
 
-io.on('connection', (socket) => {
-    const { userId, isAdmin } = socket.handshake.query;
-    require('./routes-socket')(
-        io,
-        socket,
-        userId ? parseInt(userId) : null,
-        isAdmin === 'true'
+    cluster.on(`exit`, (worker) => {
+        console.log(`Worker ${worker.process.pid} died `);
+        cluster.fork();
+    });
+} else {
+    io.adapter(redisAdapter(REDIS_URL));
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(
+        cors({
+            origin: [process.env.WEB_CLIENT_URL],
+        })
     );
-});
+    app.use(helmet());
 
-app.use(errorHandler);
+    const errorHandler = (err, req, res, next) => {
+        if (err.status === null || err.status === undefined) {
+            res.status(500).send(err.message);
+        } else {
+            res.status(err.status).send(err.message);
+        }
+    };
 
-server.listen(PORT, () => {
-    console.log('Application lancée sur le port ' + PORT);
-});
+    require('./passport');
+    require('./routes-api')(app);
+
+    io.on('connection', (socket) => {
+        const { userId, isAdmin } = socket.handshake.query;
+        require('./routes-socket')(
+            io,
+            socket,
+            userId ? parseInt(userId) : null,
+            isAdmin === 'true'
+        );
+    });
+
+    app.use(errorHandler);
+
+    server.listen(PORT, () => {
+        console.log('Application lancée sur le port ' + PORT);
+    });
+}
