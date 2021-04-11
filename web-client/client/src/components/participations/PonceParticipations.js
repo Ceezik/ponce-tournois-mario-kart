2286d9b1010fa61b747f7fd5a3a30008c430e212
Ceezik <ceezik.pro@gmail.com>
2021-04-11 11:52:47 +0200

    WEB CLIENT : admin can edit ponce participations

--- web-client/client/src/components/participations/PonceParticipations.js
+++ web-client/client/src/components/participations/PonceParticipations.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { useSelector } from 'react-redux';
 import Participations from './Participations';
 
 function PonceParticipations() {
+    const { user } = useSelector((state) => state.auth);
+
     return (
         <>
             <Helmet>
                 <title>Historique</title>
             </Helmet>
 
-            <Participations canAdd={false} route="getPonceParticipations" />
+            <Participations
+                canAdd={!!user?.isAdmin}
+                route="getPonceParticipations"
+            />
         </>
     );
 }
