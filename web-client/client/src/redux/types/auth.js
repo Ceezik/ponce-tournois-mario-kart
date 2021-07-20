dba0e62e84fd4c270988efb1466ddafbae725e70
ceezik <ceezik.pro@gmail.com>
2021-07-20 20:49:52 +0200

    WEB CLIENT: fix same reducer type for ponce and user loading

--- web-client/client/src/redux/types/auth.js
+++ web-client/client/src/redux/types/auth.js
@@ -1,4 +1,4 @@
 export const SET_USER = 'SET_USER';
-export const SET_LOADING = 'SET_LOADING';
+export const SET_LOADING = 'SET_USER_LOADING';
 export const ADD_EDITOR = 'ADD_EDITOR';
 export const REMOVE_EDITOR = 'REMOVE_EDITOR';
